3e7cbf2cffc993a3108b3c67830077dcdc4786b2
attaryu <mattarannaufal@gmail.com>
2025-02-11 17:53:18 +0700

    feat: add useArtificialLoading hook for simulating loading state

--- src/app/(workspace)/reveal-2/page.tsx
+++ src/app/(workspace)/reveal-2/page.tsx
@@ -5,116 +5,117 @@ import type { DotLottie } from '@lottiefiles/dotlottie-react';
 import { useGSAP } from '@gsap/react';
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import gsap from 'gsap';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
+
+import { useArtificialLoading } from '@/hooks/useArtificialLoading';
 
 gsap.registerPlugin(useGSAP);
 
 export default function Page() {
-	const [loading, setLoading] = useState(0);
-	const [animatedElement, setAnimatedElement] = useState<DotLottie | null>(
-		null
-	);
-	const counterElement = useRef<HTMLSpanElement | null>(null);
+  const { loadingElementRef, isLoading } = useArtificialLoading();
+  const [animatedElement, setAnimatedElement] = useState<DotLottie | null>(
+    null,
+  );
 
-	function updateCounter() {
-		setLoading((lastLoading) => {
-			const left = 100 - lastLoading;
+  useGSAP(() => {
+    if (!isLoading) {
+      // play logo animation
+      setTimeout(() => {
+        animatedElement?.play();
+      }, 400);
 
-			if (left <= 10) {
-				return lastLoading + left;
-			} else {
-				return lastLoading + Math.floor(Math.random() * 10) + 1;
-			}
-		});
-	}
+      animatedElement?.addEventListener('complete', () => {
+        gsap
+          .timeline()
+          // Hide the final animation form of the logo
+          .to('.lottie', { display: 'none', duration: 0 })
+          // Change the final animation form of the logo with a static logo similar to it
+          .to('.zoomer', { display: 'block', duration: 0 })
+          // Zoom it
+          .to('.logo-container', {
+            width: '750%',
+            duration: 2,
+            ease: 'power4.inOut',
+          })
+          // While zooming svg logo, fade out loading container
+          .to('.loading-container', { autoAlpha: 0 }, '<80%')
+          .to('.loading-container', { display: 'none' })
+          .to('.outer-container', {
+            overflow: 'auto',
+            height: 'auto',
+            duration: 0,
+          });
+      });
+    }
 
-	useEffect(() => {
-		if (window) {
-			counterElement.current!.innerHTML = loading.toString();
+    return () => {
+      animatedElement && animatedElement.removeEventListener('complete');
+    };
+  }, [isLoading]);
 
-			setTimeout(() => {
-				if (loading <= 100) {
-					updateCounter();
-				}
-			}, Math.floor(Math.random() * 600) + 1);
-		}
-	}, [loading]);
+  return (
+    <main className="outer-container w-full h-svh overflow-hidden relative">
+      {/* loading layer */}
+      <div className="loading-container absolute top-0 left-0 bg-zinc-950 h-screen w-full flex justify-center items-center flex-col overflow-hidden">
+        <div className="logo-container w-1/6">
+          <DotLottieReact
+            src="/logo-2.json"
+            speed={0.9}
+            dotLottieRefCallback={(e) => setAnimatedElement(e)}
+            className="lottie"
+          />
 
-	useGSAP(() => {
-		if (loading === 100) {
-			setTimeout(() => {
-				animatedElement?.play();
-			}, 400);
+          <img
+            src="/logo-after.svg"
+            alt=""
+            className="zoomer w-full hidden"
+          />
+        </div>
 
-			animatedElement?.addEventListener('complete', () => {
-				gsap
-					.timeline()
-					.to('.lottie', { display: 'none', duration: 0 })
-					.to('.zoomer', { display: 'block', duration: 0 })
-					.to('.logo-container', {
-						width: '750%',
-						duration: 2,
-						ease: 'power4.inOut',
-					})
-					.to('.loading-container', { autoAlpha: 0 }, '<80%')
-					.to('.loading-container', { display: 'none' })
-					.to('.outer-container', {
-						overflow: 'auto',
-						height: 'auto',
-						duration: 0,
-					});
-			});
-		}
+        <p className="text-white text-xl mt-10 font-bebas-neue">
+          <span ref={loadingElementRef}>0</span>
+          <span>%</span>
+        </p>
+        <p className="text-white font-poppins">pretend to load...</p>
+      </div>
 
-		return () => {
-			animatedElement && animatedElement.removeEventListener('complete');
-		};
-	}, [loading]);
+      {/* main layer */}
+      <div className="container mx-auto px-4 pb-10">
+        <nav className="w-full flex justify-between py-4">
+          <a
+            href="/#"
+            className="font-poppins"
+          >
+            Project
+          </a>
+          <a
+            href="/#"
+            className="font-poppins"
+          >
+            About
+          </a>
+          <a
+            href="/#"
+            className="font-poppins"
+          >
+            Contact
+          </a>
+        </nav>
 
-	return (
-		<main className="outer-container w-full h-svh overflow-hidden relative">
-			{/* loading layer */}
-			<div className="loading-container absolute top-0 left-0 bg-zinc-950 h-screen w-full flex justify-center items-center flex-col overflow-hidden">
-				<div className="logo-container w-1/6">
-					<DotLottieReact
-						src="/logo-2.json"
-						speed={0.9}
-						dotLottieRefCallback={(e) => setAnimatedElement(e)}
-						className="lottie"
-					/>
-
-					<img src="/logo-after.svg" alt="" className="zoomer w-full hidden" />
-				</div>
-
-				<p className="text-white text-xl mt-10 font-bebas-neue">
-					<span ref={counterElement}>0</span>
-					<span>%</span>
-				</p>
-				<p className="text-white font-poppins">pretend to load...</p>
-			</div>
-
-			{/* main layer */}
-			<div className="container mx-auto px-4 pb-10">
-				<nav className="w-full flex justify-between py-4">
-					<a href="/#" className="font-poppins">Project</a>
-					<a href="/#" className="font-poppins">About</a>
-					<a href="/#" className="font-poppins">Contact</a>
-				</nav>
-
-				<div className="w-full">
-					<img
-						src="/test.jpg"
-						alt=""
-						className="aspect-[8/2] w-full object-cover mb-8"
-					/>
-					<h1 className="font-bebas-neue text-[18vw] leading-none">
-						MALEFI STUDIO.
-					</h1>
-					<p className="text-[2vw] -mt-6 font-poppins">
-						Transforming Art into Digital Mastery
-					</p>
-				</div>
-			</div>
-		</main>
-	);
+        <div className="w-full">
+          <img
+            src="/test.jpg"
+            alt=""
+            className="aspect-[8/2] w-full object-cover mb-8"
+          />
+          <h1 className="font-bebas-neue text-[18vw] leading-none">
+            MALEFI STUDIO.
+          </h1>
+          <p className="text-[2vw] -mt-6 font-poppins">
+            Transforming Art into Digital Mastery
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
--- src/app/(workspace)/reveal/page.tsx
+++ src/app/(workspace)/reveal/page.tsx
@@ -1,140 +1,140 @@
 'use client';
 
+import { useArtificialLoading } from '@/hooks/useArtificialLoading';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
-import { useRef, useState } from 'react';
 
 gsap.registerPlugin(useGSAP);
 
 export default function Page() {
-	const [loading, setLoading] = useState(0);
-	const counterRef = useRef<HTMLSpanElement | null>(null);
+  const { loadingElementRef, isLoading } = useArtificialLoading();
 
-	function setCounter() {
-		const loadingLeft = 100 - loading;
+  useGSAP(() => {
+    setTimeout(() => {
+      if (!isLoading) {
+        const barAnimation: GSAPTweenVars = {
+          height: 0,
+          duration: 1,
+          ease: 'power4.inOut',
+        };
 
-		if (loadingLeft <= 10) {
-			setLoading(loading + loadingLeft);
-		} else {
-			setLoading(loading + (Math.floor(Math.random() * 10) + 1));
-		}
-	}
+        gsap
+          .timeline()
+          // hiding loading element
+          .to('.loading', { opacity: 0, duration: 0.8 })
+          // animate bar elements
+          .to('.bar-1', barAnimation, '<40%')
+          .to('.bar-2', barAnimation, '<10%')
+          .to('.bar-3', barAnimation, '<10%')
+          .to('.bar-4', barAnimation, '<10%')
+          .to('.bar-5', barAnimation, '<10%')
+          .to('.bar-6', barAnimation, '<10%')
+          .to('.bar-7', barAnimation, '<10%')
+          .to('.bar-8', barAnimation, '<10%')
+          .to('.bar-9', barAnimation, '<10%')
+          .to('.bar-10', barAnimation, '<10%')
+          // animate title elements
+          .fromTo(
+            '.h1',
+            { yPercent: 100 },
+            {
+              yPercent: 0,
+              ease: 'power2.out',
+              stagger: {
+                amount: 0.6,
+              },
+            },
+            '<20%',
+          );
+      }
+    }, 500);
+  }, [isLoading]);
 
-	useGSAP(() => {
-		if (window) {
-			setCounter();
-		}
-	}, []);
+  return (
+    <main className="w-full h-svh overflow-hidden">
+      <p className="loading flex z-20 fixed inset-0 text-white justify-end items-end p-12 font-bebas-neue font-bold">
+        <span
+          ref={loadingElementRef}
+          className="text-8xl"
+        >
+          0
+        </span>
+        <span className="text-4xl mb-2">%</span>
+      </p>
 
-	useGSAP(() => {
-		if (loading === 100) {
-			const barAnimation: GSAPTweenVars = {
-				height: 0,
-				duration: 1,
-				ease: 'power4.inOut',
-			};
+      <h1 className="flex z-10 fixed inset-0">
+        <div className="bar-1 w-[10%] h-[100vh] bg-zinc-900" />
+        <div className="bar-2 w-[10%] h-[100vh] bg-zinc-900" />
+        <div className="bar-3 w-[10%] h-[100vh] bg-zinc-900" />
+        <div className="bar-4 w-[10%] h-[100vh] bg-zinc-900" />
+        <div className="bar-5 w-[10%] h-[100vh] bg-zinc-900" />
+        <div className="bar-6 w-[10%] h-[100vh] bg-zinc-900" />
+        <div className="bar-7 w-[10%] h-[100vh] bg-zinc-900" />
+        <div className="bar-8 w-[10%] h-[100vh] bg-zinc-900" />
+        <div className="bar-9 w-[10%] h-[100vh] bg-zinc-900" />
+        <div className="bar-10 w-[10%] h-[100vh] bg-zinc-900" />
+      </h1>
 
-			gsap
-				.timeline()
-				.to('.loading', { opacity: 0, duration: 0.4 })
-				.to('.bar-1', barAnimation, '>+100%')
-				.to('.bar-2', barAnimation, '<10%')
-				.to('.bar-3', barAnimation, '<10%')
-				.to('.bar-4', barAnimation, '<10%')
-				.to('.bar-5', barAnimation, '<10%')
-				.to('.bar-6', barAnimation, '<10%')
-				.to('.bar-7', barAnimation, '<10%')
-				.to('.bar-8', barAnimation, '<10%')
-				.to('.bar-9', barAnimation, '<10%')
-				.to('.bar-10', barAnimation, '<10%')
-				.fromTo(
-					'.h1',
-					{ yPercent: 100 },
-					{
-						yPercent: 0,
-						ease: 'power2.out',
-						stagger: {
-							amount: 0.6,
-						},
-					},
-					'<20%'
-				);
-		}
+      <div className="container mx-auto relative">
+        <nav className="sticky top-0 flex justify-between py-6">
+          <a
+            href="/#"
+            className="font-poppins"
+          >
+            About
+          </a>
+          <a
+            href="/#"
+            className="font-poppins"
+          >
+            Contact
+          </a>
+          <a
+            href="/#"
+            className="font-poppins"
+          >
+            Playground
+          </a>
+        </nav>
 
-		if (window && loading) {
-			counterRef.current!.innerHTML = loading.toString();
+        <div className="w-full flex justify-center h-fit overflow-hidden selection:bg-zinc-900 selection:text-white">
+          <span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
+            F
+          </span>
+          <span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
+            A
+          </span>
+          <span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
+            R
+          </span>
+          <span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
+            I
+          </span>
+          <span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
+            E
+          </span>
+          <span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
+            T
+          </span>
+          <span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
+            T
+          </span>
+          <span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
+            O
+          </span>
+          <span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
+            .
+          </span>
+        </div>
 
-			setTimeout(() => {
-				if (loading <= 100) {
-					setCounter();
-				}
-			}, Math.floor(Math.random() * 500) + 1);
-		}
-	}, [loading]);
-
-	return (
-		<main className="w-full h-svh overflow-hidden">
-			<p className="loading flex z-20 fixed inset-0 text-white justify-end items-end p-12 font-bebas-neue font-bold">
-				<span ref={counterRef} className="text-8xl">
-					0
-				</span>
-				<span className="text-4xl mb-2">%</span>
-			</p>
-
-			<h1 className="flex z-10 fixed inset-0">
-				<div className="bar-1 w-[10%] h-[100vh] bg-zinc-900" />
-				<div className="bar-2 w-[10%] h-[100vh] bg-zinc-900" />
-				<div className="bar-3 w-[10%] h-[100vh] bg-zinc-900" />
-				<div className="bar-4 w-[10%] h-[100vh] bg-zinc-900" />
-				<div className="bar-5 w-[10%] h-[100vh] bg-zinc-900" />
-				<div className="bar-6 w-[10%] h-[100vh] bg-zinc-900" />
-				<div className="bar-7 w-[10%] h-[100vh] bg-zinc-900" />
-				<div className="bar-8 w-[10%] h-[100vh] bg-zinc-900" />
-				<div className="bar-9 w-[10%] h-[100vh] bg-zinc-900" />
-				<div className="bar-10 w-[10%] h-[100vh] bg-zinc-900" />
-			</h1>
-
-			<div className="container mx-auto relative">
-				<nav className="sticky top-0 flex justify-between py-6">
-					<a href="/#" className="font-poppins">About</a>
-					<a href="/#" className="font-poppins">Contact</a>
-					<a href="/#" className="font-poppins">Playground</a>
-				</nav>
-
-				<div className="w-full flex justify-center h-fit overflow-hidden selection:bg-zinc-900 selection:text-white">
-					<span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
-						F
-					</span>
-					<span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
-						A
-					</span>
-					<span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
-						R
-					</span>
-					<span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
-						I
-					</span>
-					<span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
-						E
-					</span>
-					<span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
-						T
-					</span>
-					<span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
-						T
-					</span>
-					<span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
-						O
-					</span>
-					<span className="h1 font-bebas-neue font-bold text-[15rem] leading-tight">
-						.
-					</span>
-				</div>
-
-				<div className="w-full aspect-[6/2] mx-auto rounded-xl">
-					<img src="/test.jpg" alt="" className="w-full h-full object-cover" />
-				</div>
-			</div>
-		</main>
-	);
+        <div className="w-full aspect-[6/2] mx-auto rounded-xl">
+          <img
+            src="/test.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
